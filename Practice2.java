85eeeb7caf0f124363cfb8f97314553ffe34e2fb
Karl Bae <karl.bae@gmail.com>
2016-06-15 12:01:33 +1000

    Base 7 function
    function convert translate given long to base 7 String.

---
+++ Practice2.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Min on 15/06/2016.
+ */
+import java.math.*;
+import java.util.regex.*;
+
+public class Practice2 {
+    /// to convert to base 7 from given base 10
+    public static String convert(long _in) {
+        char [] base7={'0','S','y','d','n','e','y'};
+        StringBuffer target=new StringBuffer();
+
+        for (long i=_in; i>0; i /=7){
+            target.append(base7[(int)i%7]);
+        }
+
+        target= target.reverse();
+        return target.toString();
+    }
+
+    public static void main (String[] args) throws Exception {
+        long _in=7;
+        _in=16807+2401*2+343*3+49*4+7*5+1*6;
+        System.out.println(convert(_in));
+    }
+}
